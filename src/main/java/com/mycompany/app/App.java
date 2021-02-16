36a83ad87518918322dce743af5880ecc45d2c88
yasarlaro <yasarlar_onur@hotmail.com>
2021-02-16 17:20:55 +0100

    krakow added to message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Poland!" );
+        System.out.println( "Hello World from Krakow / Poland!" );
     }
 }
